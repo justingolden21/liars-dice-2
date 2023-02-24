11e8b078a8193f61b35f770676b91f781efce502
Justin Golden <justingolden21@gmail.com>
2023-02-23 19:52:43 -0800

    Hide blue square on first load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,16 +16,18 @@
 <div class="container mx-auto sm:p-8 h-screen flex flex-col items-center justify-between gap-4">
 	<h1 class="h1 text-center mb-10">Liar's Dice</h1>
 
-	<div class="bg-blue-900 text-white p-4 sm:p-10">
-		{#if $game.state === 'passPhone'}
-			<PassThePhone />
-		{:else if $game.state === 'playerTurn'}
-			<PlayerDashboard />
-		{:else if $game.bet.face !== 1}
-			<!-- 'called', 'spotted', or 'gameOver' -->
-			<RoundEnd />
-		{/if}
-	</div>
+	{#if $game.bet.face !== 1 || $game.state !== 'gameOver'}
+		<div class="bg-blue-900 text-white p-4 sm:p-10">
+			{#if $game.state === 'passPhone'}
+				<PassThePhone />
+			{:else if $game.state === 'playerTurn'}
+				<PlayerDashboard />
+			{:else if $game.bet.face !== 1}
+				<!-- 'called', 'spotted', or 'gameOver' -->
+				<RoundEnd />
+			{/if}
+		</div>
+	{/if}
 
 	<NewGameButton />
 </div>
