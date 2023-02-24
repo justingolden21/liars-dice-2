3148f400717163b5cc9e2f71e7374f52fb31bda5
Justin Golden <justingolden21@gmail.com>
2023-02-23 18:59:14 -0800

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+	/**
+	 * Used for 404, 5xx, and other errors
+	 */
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>Error | Liar's Dice</title>
+</svelte:head>
+
+<div class="container mx-auto sm:p-8">
+	<h1 class="h1">Error {$page.status} &mdash; {$page.error.message}</h1>
+
+	<a href="/" class="block button -primary">Go home</a>
+</div>
